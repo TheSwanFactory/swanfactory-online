f4e6a9dde7423a1c3edae3288497d9644883391e
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-01-18 20:49:46 -0800

    fmt

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,14 +7,14 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
-  <body>
-    <main>
+      <body>
+        <main>
           <Component />
-    </main>
-    <footer class="site-footer">
-      <p>&copy; 2025 The Swan Factory</p>
-    </footer>
-  </body>
+        </main>
+        <footer class="site-footer">
+          <p>&copy; 2025 The Swan Factory</p>
+        </footer>
+      </body>
     </html>
   );
 }

607f9a4350af1ee5d27c015afbbafea672dcebb1
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-02-09 17:44:24 -0800

    cleanup

--- utils/models.test.ts
+++ utils/models.test.ts
@@ -1,30 +1,22 @@
 import { assertEquals, assertNotEquals } from "jsr:@std/assert";
 import { db, User } from "./models.ts";
 
-// Test UserModel
-
 Deno.test("Upsert User into db", async () => {
-  const user: User = {
-    realm: "test.user",
-  };
+  const user: User = { realm: "test.user" };
 
   const result = await db.users.upsertByPrimaryIndex({
     index: ["realm", user.realm],
     update: user,
     set: user,
   });
-  console.log(result);
   assertEquals(result.ok, true);
 
-  // getOne
   const one = await db.users.getOne();
-  console.log(one);
   assertNotEquals(one, null);
   assertEquals(one?.value.realm, user.realm);
 
-  const user_id = one?.id;
-  if (user_id !== undefined) {
-    await db.users.delete(user_id);
+  if (one?.id !== undefined) {
+    await db.users.delete(one.id);
   } else {
     throw new Error("user_id is undefined");
   }
